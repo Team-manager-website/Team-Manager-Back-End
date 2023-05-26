f7271a60ae84fe3022497d4d20ed97912075cc8a
Berden,Cédric C.P.J <c.berden@student.fontys.nl>
2023-05-26 11:07:07 +0200

    Test 79

--- src/test/java/com/example/teammanagerapi/controller/TeamControllerTest.java
+++ src/test/java/com/example/teammanagerapi/controller/TeamControllerTest.java
@@ -1,25 +1,14 @@
 package com.example.teammanagerapi.controller;
 
 import com.example.teammanagerapi.dal.interfacesjpa.ITeamRepository;
-import com.example.teammanagerapi.dal.jpa.Team;
-import com.mashape.unirest.http.Unirest;
-import jakarta.annotation.PostConstruct;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.net.http.HttpResponse;
 
@@ -45,7 +34,7 @@ class TeamControllerTest {
     void getTeamName() throws Exception {
         String teamName = "{\"input\":\"VVV\"}";
         mockMvc.perform(post("/team/CreateTeam")
-                        .header("authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkFpc0xNdnJPcklGa2MwZjQ1UzJ4ZyJ9.eyJpc3MiOiJodHRwczovL2Rldi1nbTJmM29iei51cy5hdXRoMC5jb20vIiwic3ViIjoiNUNLVDd5aFFnOWJyUXViOTIydjVBVnQyWmJnVTEzUUxAY2xpZW50cyIsImF1ZCI6Imh0dHA6Ly90ZWFtbWFuYWdlci9hcGkiLCJpYXQiOjE2ODQ5Mjc0MTcsImV4cCI6MTY4NTAxMzgxNywiYXpwIjoiNUNLVDd5aFFnOWJyUXViOTIydjVBVnQyWmJnVTEzUUwiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.dljSNr2_RfTAnZqjLF5JXBewYFkA5Rt_NbECmCbcJmyaCB7jVzS9FR61McSyJ3WmzkFBuP3GHSGIvxR3MdVewC_l-4Yj--jywR3qtbbjraUrqfYiCcbXR0I7nsXur3Be-HCPeqtUdK9uzpsOHU3SdHz4T7bVaI0q_aZ_WDfKvkWSZhuvC5crYyqztOrV8y_VsHIRUAQ81j3IgCCxnh3HRrfWnnphndUP1qtMHD8S8PYv1Xfs6pizAbHzp96WVwMC3Rid-AbQ25lv_tALZvIdHqsvBfXTG8h6_Pq1pOGAUc1NHH4WGxntZENsGSlbzLOETd6cR8rPabKgkDUQiD6TvA")
+                        .header("authorization", "Bearer eyJhbGciOiJSUI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkFpc0xNdnJPcklGa2MwZjQ1UzJ4ZyJ9.eyJpc3MiOiJodHRwczovL2Rldi1nbTJmM29iei51cy5hdXRoMC5jb20vIiwic3ViIjoiNUNLVDd5aFFnOWJyUXViOTIydjVBVnQyWmJnVTEzUUxAY2xpZW50cyIsImF1ZCI6Imh0dHA6Ly90ZWFtbWFuYWdlci9hcGkiLCJpYXQiOjE2ODUwODc2MDMsImV4cCI6MTY4NTE3NDAwMywiYXpwIjoiNUNLVDd5aFFnOWJyUXViOTIydjVBVnQyWmJnVTEzUUwiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.NufZd0DaSVFwSVOAq42I4QJn-aAW669UsVrDTZ9e0kQZtjlIGqbv5sW5-sWqjmSY2lKaiqrzuCPkwmnWvABjkLePJ1CUF7JvfWm3-Kzj1YylfDnm0FgD-K0v-MjW_zEFOG6egJSpQEc0c1ebThLjr15JcqsxYFXkhHcGjmvGFE3naPtn50RHLqq4I5rbWhO2LAiPTJ7g-6cmaGDTncdIgAwSvc1uqYlAuB0FkKbdWTYgn-o7r7Ajb08PbXAb8jSfqpMwGwnPVLo4sISzMOnHREUVLdVCnO8U53GekD8Q_iHDOljSvvvzoAkdwZ4zdqc6UDqe4ty8D4Y4whJowMwVPg")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(teamName))
                 .andExpect(status().isOk());
