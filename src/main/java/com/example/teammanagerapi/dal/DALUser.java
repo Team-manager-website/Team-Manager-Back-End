2c9c2dc646a85be046d42fff4e116ddd2294833e
Berden,Cédric C.P.J <c.berden@student.fontys.nl>
2023-06-10 11:02:08 +0200

    Test 135

--- src/main/java/com/example/teammanagerapi/dal/DALUser.java
+++ src/main/java/com/example/teammanagerapi/dal/DALUser.java
@@ -1,17 +1,17 @@
 package com.example.teammanagerapi.dal;
 
 import com.example.teammanagerapi.dal.interfaces.IDALUserCollection;
-//import com.example.teammanagerapi.dal.interfacesjpa.IUserRepository;
-//import com.example.teammanagerapi.dal.jpa.User;
+import com.example.teammanagerapi.dal.interfacesjpa.IUserRepository;
+import com.example.teammanagerapi.dal.jpa.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DALUser implements IDALUserCollection {
-//    @Autowired
-//    private IUserRepository userRepository;
+    @Autowired
+    private IUserRepository userRepository;
     @Override
     public void createUser(String userName, String userId) {
-//        userRepository.save(new User(userId, userName));
+        userRepository.save(new User(userId, userName));
     }
 }
--- src/main/java/com/example/teammanagerapi/dal/interfacesjpa/ITeamRepository.java
+++ src/main/java/com/example/teammanagerapi/dal/interfacesjpa/ITeamRepository.java
@@ -1,13 +1,13 @@
-//package com.example.teammanagerapi.dal.interfacesjpa;
-//
-//import com.example.teammanagerapi.dal.jpa.Team;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ITeamRepository extends JpaRepository<Team, Integer> {
-//    @Query(value = "SELECT team_name FROM team WHERE team_name = :name ORDER BY id DESC LIMIT 1", nativeQuery=true)
-//    String findByTeamName(@Param("name") String name);
-//}
+package com.example.teammanagerapi.dal.interfacesjpa;
+
+import com.example.teammanagerapi.dal.jpa.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITeamRepository extends JpaRepository<Team, Integer> {
+    @Query(value = "SELECT team_name FROM team WHERE team_name = :name ORDER BY id DESC LIMIT 1", nativeQuery=true)
+    String findByTeamName(@Param("name") String name);
+}
