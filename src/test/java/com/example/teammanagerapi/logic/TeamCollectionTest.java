f2b8cb6faa20412f74368ba3acca227bf52124ae
Berden,Cédric C.P.J <c.berden@student.fontys.nl>
2023-06-10 10:57:14 +0200

    Test 133

--- src/test/java/com/example/teammanagerapi/logic/TeamCollectionTest.java
+++ src/test/java/com/example/teammanagerapi/logic/TeamCollectionTest.java
@@ -1,31 +1,31 @@
-package com.example.teammanagerapi.logic;
-
-import com.example.teammanagerapi.dal.interfacesjpa.ITeamRepository;
-import com.example.teammanagerapi.dal.jpa.Team;
-import jakarta.annotation.PostConstruct;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class TeamCollectionTest {
-    private TestRestTemplate restTemplate;
-
-    private ITeamRepository teamRepository;
-
-//    @Test
-//    void createTeam() {
-//        String teamName = "VVV";
+//package com.example.teammanagerapi.logic;
 //
-//        ResponseEntity<Void> response = restTemplate.postForEntity("/team/CreateTeam", teamName, Void.class);
+//import com.example.teammanagerapi.dal.interfacesjpa.ITeamRepository;
+//import com.example.teammanagerapi.dal.jpa.Team;
+//import jakarta.annotation.PostConstruct;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
 //
-//        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+//import static org.junit.jupiter.api.Assertions.*;
 //
-//        String savedTeam = teamRepository.findByTeamName("VVV");
-//        assertNotNull(savedTeam);
-//        assertEquals(teamName, savedTeam.getTeamName());
-//    }
-}
+//class TeamCollectionTest {
+//    private TestRestTemplate restTemplate;
+//
+//    private ITeamRepository teamRepository;
+//
+////    @Test
+////    void createTeam() {
+////        String teamName = "VVV";
+////
+////        ResponseEntity<Void> response = restTemplate.postForEntity("/team/CreateTeam", teamName, Void.class);
+////
+////        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+////
+////        String savedTeam = teamRepository.findByTeamName("VVV");
+////        assertNotNull(savedTeam);
+////        assertEquals(teamName, savedTeam.getTeamName());
+////    }
+//}
