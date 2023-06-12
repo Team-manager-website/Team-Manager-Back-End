6a0376b941f8438e249f5f32e9efb0e81fba47a7
Berden,Cédric C.P.J <c.berden@student.fontys.nl>
2023-06-12 22:21:47 +0200

    Test 140

--- src/main/java/com/example/teammanagerapi/controller/AccountController.java
+++ src/main/java/com/example/teammanagerapi/controller/AccountController.java
@@ -1,10 +1,8 @@
 package com.example.teammanagerapi.controller;
 
 import com.example.teammanagerapi.logic.interfaces.IUserCollection;
-//import jakarta.persistence.Access;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
